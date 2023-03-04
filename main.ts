d3f9e94af0459a96fdefe8b28a457707f4d25fee
MorisakiYuji <youkourow@yahoo.co.jp>
2023-03-04 12:53:31 +0900

    Stad: Promise.all

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
-import * as fs from 'fs'
-const num = 0
-
-console.log(num, fs);
+const allPromised = Promise.all([
+  Promise.resolve('foo'),
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve('timer')
+    }, 1000)
+  }).then(result => console.log(result)),
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve('timer')
+    }, 2000)
+  }).then(result => console.log(result)),
+  // Promise.reject('error')
+]).then(() => {
+  console.log('end')
+}).catch(() => {
+  console.error('error')
+})
