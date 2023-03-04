96f3d1c4108af0baeb9ace70ba357623c42e974c
MorisakiYuji <youkourow@yahoo.co.jp>
2023-03-04 13:30:36 +0900

    Stad: Promise.race

--- main.ts
+++ main.ts
@@ -1,18 +1,21 @@
-const allPromised = Promise.all([
-  Promise.resolve('foo'),
-  new Promise(resolve => {
-    setTimeout(() => {
-      resolve('timer')
-    }, 1000)
-  }).then(result => console.log(result)),
-  new Promise(resolve => {
-    setTimeout(() => {
-      resolve('timer')
-    }, 2000)
-  }).then(result => console.log(result)),
-  // Promise.reject('error')
-]).then(() => {
-  console.log('end')
-}).catch(() => {
-  console.error('error')
-})
+const withTimeout = (promises: Promise<unknown>[], timer: number) => {
+  return Promise.race([
+    ...promises,
+    new Promise((_, reject) => {
+      setTimeout(() => {
+        reject(new Error('Timeout'))
+      }, timer) 
+    })
+  ]).then(result => {
+    console.log(result)
+  }).catch(err => {
+    console.error(err)
+  })
+}
+
+const promises = [
+  new Promise<string>(resolve => setTimeout(() => resolve('do'), 2000)),
+  new Promise<number>(resolve => setTimeout(() => resolve(10), 2000)),
+]
+
+withTimeout(promises, 2000)
