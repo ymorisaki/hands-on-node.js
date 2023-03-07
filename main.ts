5da314867b094dfc941e589d4de4b156ad789c44
MorisakiYuji <youkourow@yahoo.co.jp>
2023-03-07 14:01:55 +0900

    Stad: Basic Stream

--- main.ts
+++ main.ts
@@ -1,60 +1,11 @@
-import * as events from 'node:events'
+import * as fs from 'fs'
+import * as crypt from 'crypto'
 
-function createFizzBuzzEventEmitter(until: number) {
-  const eventEmitter = new events.EventEmitter()
-
-  process.nextTick(() => _emitFizzBuzz(eventEmitter, until))
-  return eventEmitter
+const readStream = (src: string, dest: string, cb: () => void) => {
+  fs.createReadStream(src)
+    .pipe(crypt.createHash('sha256'))
+    .pipe(fs.createWriteStream(dest))
+    .on('finish', cb)
 }
 
-async function _emitFizzBuzz(eventEmitter: events.EventEmitter, until: number) {
-  let count = 1
-
-  while (count <= until) {
-    await new Promise(resolve => setTimeout(resolve, 100))
-
-    if (count % 15 === 0) {
-      eventEmitter.emit('FizzBuzz', count)
-    } else if (count % 3 === 0) {
-      eventEmitter.emit('Fizz', count)
-    } else if (count % 5 === 0) {
-      eventEmitter.emit('Buzz', count)
-    }
-    count += 1
-  }
-
-  eventEmitter.emit('end', eventEmitter)
-}
-
-function startListener() {
-  console.log('start')
-}
-
-function fizzListener(count: number) {
-  console.log('Fizz', count)
-}
-
-function buzzListener(count: number) {
-  console.log('Buzz', count)
-}
-
-function fizzBuzzListener(count: number) {
-  console.log('FizzBuzz', count)
-}
-
-function endListener(eventEmitter: events.EventEmitter) {
-  console.log('end')
-  eventEmitter
-    .off('start', startListener)
-    .off('Fizz', fizzListener)
-    .off('Buzz', buzzListener)
-    .off('FizzBuzz', fizzBuzzListener)
-    .off('end', endListener)
-}
-
-createFizzBuzzEventEmitter(40)
-  .on('start', startListener)
-  .on('Fizz', fizzListener)
-  .on('Buzz', buzzListener)
-  .on('FizzBuzz', fizzBuzzListener)
-  .on('end', endListener)
+readStream('src.txt', 'dest.txt', () => console.log('copy end'))
