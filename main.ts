7a9491449a1a7a6325d8ea8bae0e80650ab84d94
MorisakiYuji <youkourow@yahoo.co.jp>
2023-03-07 12:51:03 +0900

    Stad: FizzBuzz Game

--- main.ts
+++ main.ts
@@ -1,21 +1,60 @@
-const withTimeout = (promises: Promise<unknown>[], timer: number) => {
-  return Promise.race([
-    ...promises,
-    new Promise((_, reject) => {
-      setTimeout(() => {
-        reject(new Error('Timeout'))
-      }, timer) 
-    })
-  ]).then(result => {
-    console.log(result)
-  }).catch(err => {
-    console.error(err)
-  })
+import * as events from 'node:events'
+
+function createFizzBuzzEventEmitter(until: number) {
+  const eventEmitter = new events.EventEmitter()
+
+  process.nextTick(() => _emitFizzBuzz(eventEmitter, until))
+  return eventEmitter
 }
 
-const promises = [
-  new Promise<string>(resolve => setTimeout(() => resolve('do'), 2000)),
-  new Promise<number>(resolve => setTimeout(() => resolve(10), 2000)),
-]
+async function _emitFizzBuzz(eventEmitter: events.EventEmitter, until: number) {
+  let count = 1
 
-withTimeout(promises, 2000)
+  while (count <= until) {
+    await new Promise(resolve => setTimeout(resolve, 100))
+
+    if (count % 15 === 0) {
+      eventEmitter.emit('FizzBuzz', count)
+    } else if (count % 3 === 0) {
+      eventEmitter.emit('Fizz', count)
+    } else if (count % 5 === 0) {
+      eventEmitter.emit('Buzz', count)
+    }
+    count += 1
+  }
+
+  eventEmitter.emit('end', eventEmitter)
+}
+
+function startListener() {
+  console.log('start')
+}
+
+function fizzListener(count: number) {
+  console.log('Fizz', count)
+}
+
+function buzzListener(count: number) {
+  console.log('Buzz', count)
+}
+
+function fizzBuzzListener(count: number) {
+  console.log('FizzBuzz', count)
+}
+
+function endListener(eventEmitter: events.EventEmitter) {
+  console.log('end')
+  eventEmitter
+    .off('start', startListener)
+    .off('Fizz', fizzListener)
+    .off('Buzz', buzzListener)
+    .off('FizzBuzz', fizzBuzzListener)
+    .off('end', endListener)
+}
+
+createFizzBuzzEventEmitter(40)
+  .on('start', startListener)
+  .on('Fizz', fizzListener)
+  .on('Buzz', buzzListener)
+  .on('FizzBuzz', fizzBuzzListener)
+  .on('end', endListener)
